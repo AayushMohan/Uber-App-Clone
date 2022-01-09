5eeb1bd6038bf27f4d6f23e9b170ab41265a0072
AayushMohan <aayushmohan1702@gmail.com>
2022-01-09 21:10:40 +0530

    commit

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,30 @@
 import React from "react";
+import tw from "tailwind-styled-components";
 
 const search = () => {
-  return <div>Hello</div>;
+  return (
+    <Wrapper>
+      {/* Button Container */}
+      <ButtonContainer>
+        <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+      </ButtonContainer>
+      {/* Input Container */}
+      {/* Saved Places */}
+      {/* Confirm Location */}
+    </Wrapper>
+  );
 };
 
 export default search;
+
+const Wrapper = tw.div`
+ bg-gray-200 h-screen
+`;
+
+const ButtonContainer = tw.div`
+ bg-white px-4
+`;
+
+const BackButton = tw.img`
+ h-12
+`;
