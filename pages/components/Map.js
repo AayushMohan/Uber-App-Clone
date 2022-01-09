68070ed71f85ff8f295c5f61962920c1d7167bc7
AayushMohan <aayushmohan1702@gmail.com>
2022-01-09 14:44:03 +0530

    commit

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
 import tw from "tailwind-styled-components";
 import mapboxgl from "!mapbox-gl";
 
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWF5dXNobW9oYW4iLCJhIjoiY2t2cGVpaWlqMXNmcTJ4b3VlN2tsaGNscSJ9.3D_bhqYitsTH9fIDyg17Hw";
+
 const Map = () => {
-  return <div></div>;
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    });
+  });
+
+  return <Wrapper id="map"></Wrapper>;
 };
 
 export default Map;
+
+const Wrapper = tw.div`
+flex-1
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,12 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
-import mapboxgl from "!mapbox-gl";
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYWF5dXNobW9oYW4iLCJhIjoiY2t2cGVpaWlqMXNmcTJ4b3VlN2tsaGNscSJ9.3D_bhqYitsTH9fIDyg17Hw";
+import Map from "./components/Map";
 
 export default function Home() {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
-      center: [-99.29011, 39.39172],
-      zoom: 3,
-    });
-  });
-
   return (
     <Wrapper>
-      <Map id="map"></Map>
+      <Map />
       <ActionItems>Start</ActionItems>
     </Wrapper>
   );
@@ -27,10 +15,6 @@ export default function Home() {
 
 const Wrapper = tw.div`
   flex flex-col bg-red-300 h-screen
-`;
-
-const Map = tw.div`
-  bg-red-500 flex-1
 `;
 
 const ActionItems = tw.div`
