b924b2d81e9af3e9be975c9746ce8aa2bbbebc4c
AayushMohan <aayushmohan1702@gmail.com>
2022-01-09 22:26:57 +0530

    commit

--- pages/search.js
+++ pages/search.js
@@ -48,14 +48,20 @@ const InputContainer = tw.div`
 `;
 
 const FormToIcons = tw.div`
- w-10 flex flex-col mr-2
+ w-10 flex flex-col mr-2 items-center
 `;
 
-const Circle = tw.img``;
+const Circle = tw.img`
+  h-2.5
+`;
 
-const Line = tw.img``;
+const Line = tw.img`
+  h-10
+`;
 
-const Square = tw.img``;
+const Square = tw.img`
+  h-3
+`;
 
 const InputBoxes = tw.div`
  flex flex-col flex-1
