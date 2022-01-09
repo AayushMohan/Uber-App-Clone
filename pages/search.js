d2481c7f62f67718cbe1539b136e7eddcbe83c65
AayushMohan <aayushmohan1702@gmail.com>
2022-01-09 21:37:47 +0530

    commit

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,15 @@ const search = () => {
       <ButtonContainer>
         <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
       </ButtonContainer>
+
       {/* Input Container */}
+      <InputContainer>
+        <FormToIcons>
+          <Circle src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png" />
+          <Line src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png" />
+          <Square src="https://img.icons8.com/windows/50/000000/square-full.png" />
+        </FormToIcons>
+      </InputContainer>
       {/* Saved Places */}
       {/* Confirm Location */}
     </Wrapper>
@@ -28,3 +36,17 @@ const ButtonContainer = tw.div`
 const BackButton = tw.img`
  h-12
 `;
+
+const InputContainer = tw.div`
+ bg-white
+`;
+
+const FormToIcons = tw.div`
+ w-10 flex flex-col
+`;
+
+const Circle = tw.img``;
+
+const Line = tw.img``;
+
+const Square = tw.img``;
