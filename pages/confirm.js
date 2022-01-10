89cc4278816f39929afb69fa7e0ea997d6b5a4fb
AayushMohan <aayushmohan1702@gmail.com>
2022-01-10 20:37:50 +0530

    commit

---
+++ pages/confirm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+
+const confirm = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default confirm;
+
+const Wrapper = tw.div``;
