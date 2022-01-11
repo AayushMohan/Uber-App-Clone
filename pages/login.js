8f7cb2455f2d8163dc3ec568a1c407d31420d5c3
AayushMohan <aayushmohan1702@gmail.com>
2022-01-11 18:58:17 +0530

    commit

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,9 @@ const Login = () => {
 
 export default Login;
 
-const Wrapper = tw.div``;
+const Wrapper = tw.div`
+ flex flex-col h-screen w-screen bg-gray-200 p-4
+`;
 
 const SignInButton = tw.button`
  bg-black text-white text-center py-4 mt-8 self-center w-full 
