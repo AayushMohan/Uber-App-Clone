d419a9fa498fce783f98a5aa1852cc70f740a600
AayushMohan <aayushmohan1702@gmail.com>
2022-01-11 11:30:21 +0530

    commit

--- pages/confirm.js
+++ pages/confirm.js
@@ -3,17 +3,40 @@ import tw from "tailwind-styled-components";
 import Map from "./components/Map";
 
 const Confirm = () => {
-  const getCoordinates = () => {
+  const getPickupCoordinates = () => {
     const location = "Santa Monica";
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json`)
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1IjoiYWF5dXNobW9oYW4iLCJhIjoiY2t2cGVpaWlqMXNmcTJ4b3VlN2tsaGNscSJ9.3D_bhqYitsTH9fIDyg17Hw",
+          limit: 1,
+        })
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.features[0].center);
+      });
+  };
+
+  const getDropoffCoordinates = () => {
+    const location = "Santa Monica";
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1IjoiYWF5dXNobW9oYW4iLCJhIjoiY2t2cGVpaWlqMXNmcTJ4b3VlN2tsaGNscSJ9.3D_bhqYitsTH9fIDyg17Hw",
+          limit: 1,
+        })
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.features[0].center);
       });
   };
 
   useEffect(() => {
-    getCoordinates();
+    getPickupCoordinates();
   });
 
   return (
