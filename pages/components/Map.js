7d256c186cce5ba1820c0bba6175b7442d558b77
AayushMohan <aayushmohan1702@gmail.com>
2022-01-11 21:41:19 +0530

    commit

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -14,6 +14,18 @@ const Map = (props) => {
       zoom: 3,
     });
 
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      })
+    );
+
     if (props.pickupCoordinates) {
       addToMap(map, props.pickupCoordinates);
     }
