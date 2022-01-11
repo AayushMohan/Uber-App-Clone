d2f98c4fd63c80b20712a51d0ccc04d6f26fc9cd
AayushMohan <aayushmohan1702@gmail.com>
2022-01-11 15:02:11 +0530

    commit

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import tw from "tailwind-styled-components";
 import Link from "next/link";
 
 const search = () => {
+  const [pickup, setPickup] = useState("");
+  const [dropoff, setDropoff] = useState("");
+
+  console.log(pickup);
+  console.log(dropoff);
+
   return (
     <Wrapper>
       {/* Button Container */}
@@ -20,8 +26,16 @@ const search = () => {
           <Square src="https://img.icons8.com/windows/50/000000/square-full.png" />
         </FormToIcons>
         <InputBoxes>
-          <Input placeholder="Enter pickup location" />
-          <Input placeholder="Where to?" />
+          <Input
+            placeholder="Enter pickup location"
+            value={pickup}
+            onChange={(e) => setPickup(e.target.value)}
+          />
+          <Input
+            placeholder="Where to?"
+            value={dropoff}
+            onChange={(e) => setDropoff(e.target.value)}
+          />
         </InputBoxes>
         <PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png" />
       </InputContainer>
@@ -35,8 +49,8 @@ const search = () => {
         href={{
           pathname: "/confirm",
           query: {
-            pickup: "Santa Monica",
-            dropoff: "Los Angeles",
+            pickup: pickup,
+            dropoff: dropoff,
           },
         }}
       >
